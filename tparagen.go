d85d3c3e33f237fb0dd259a447ea177cc5ee267c
sho-hata <syouki100241@gmail.com>
2024-10-28 10:47:36 +0900

    implement all-or-nothing

--- tparagen.go
+++ tparagen.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/saracen/walker"
 )
@@ -47,7 +48,15 @@ type tparagen struct {
 	needFixLoopVar       bool
 }
 
+// run() traverses from the root node, and when it finds the target test file, it will process the assignment of a concurrency marker.
+// The contents of each processed file are written to a temporary file.
+// After all scans are complete, rewrite the original file with the contents of each temporary file.
 func (t *tparagen) run() error {
+	// Information of files to be modified
+	// key: original file path, value: temporary file path
+	// walker.Walk() may execute concurrently, so sync.Map is used.
+	var tempFiles sync.Map
+
 	err := walker.Walk(t.in, func(path string, info fs.FileInfo) error {
 		if info.IsDir() && t.skipDir(path) {
 			return filepath.SkipDir
@@ -62,6 +71,15 @@ func (t *tparagen) run() error {
 			return fmt.Errorf("cannot open %s. %w", path, err)
 		}
 		defer f.Close()
+
+		tmpf, err := os.CreateTemp("", "temp_")
+		if err != nil {
+			return fmt.Errorf("failed to create temp file for %s. %w", path, err)
+		}
+
+		defer tmpf.Close()
+		tempFiles.Store(path, tmpf.Name())
+
 		b, err := io.ReadAll(f)
 		if err != nil {
 			return fmt.Errorf("cannot read %s. %w", path, err)
@@ -80,8 +98,44 @@ func (t *tparagen) run() error {
 
 		return nil
 	})
+	// If an error occurs, remove all temporary files
+	if err != nil {
+		tempFiles.Range(func(_, p any) bool {
+			path, ok := p.(string)
+			if !ok {
+				return false
+			}
 
-	return err
+			// Remove temporary files
+			os.Remove(path)
+
+			return true
+		})
+
+		return err
+	}
+
+	// Replace the original file with the temporary file if all writes are successful
+	tempFiles.Range(func(key, value any) bool {
+		origPath, ok := key.(string)
+		if !ok {
+			return false
+		}
+
+		tmpPath, ok := value.(string)
+		if !ok {
+			return false
+		}
+
+		if err := os.Rename(tmpPath, origPath); err != nil {
+			// TODO: logging
+			t.errStream.Write([]byte(fmt.Sprintf("failed to rename %s to %s. %v\n", tmpPath, origPath, err)))
+		}
+
+		return true
+	})
+
+	return nil
 }
 
 func (t *tparagen) skipDir(p string) bool {
