f35dfc3fdc1718205809350a3149187be98edce8
sho-hata <syouki100241@gmail.com>
2024-10-28 16:03:25 +0900

    chore: rename process to GenerateTParallel

--- cmd/tparagen/main.go
+++ cmd/tparagen/main.go
@@ -22,7 +22,7 @@ func main() {
 	kingpin.HelpFlag.Short('h')
 
 	if err := tparagen.Run(os.Stdout, os.Stderr, strings.Split(*ignoreDirectories, ","), *minGoVersion); err != nil {
-		fmt.Println(err.Error())
+		os.Stderr.WriteString(err.Error())
 		os.Exit(1)
 	}
 
